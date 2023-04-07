d25c8106fbd0ed89423adafbf4aa8233ba3e57ba
thgPwls98 <127263928+thgPwls98@users.noreply.github.com>
2023-04-07 15:53:32 +0900

    Add files via upload

--- 021. Firebase/Form1.Designer.cs
+++ 021. Firebase/Form1.Designer.cs
@@ -28,21 +28,244 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblId = new System.Windows.Forms.Label();
+            this.txtId = new System.Windows.Forms.TextBox();
+            this.txtSId = new System.Windows.Forms.TextBox();
+            this.lblSId = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.txtPhone = new System.Windows.Forms.TextBox();
+            this.lblPhone = new System.Windows.Forms.Label();
+            this.btnViewAll = new System.Windows.Forms.Button();
+            this.btnDeleteAll = new System.Windows.Forms.Button();
+            this.btnInsert = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(550, 240);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // lblId
+            // 
+            this.lblId.AutoSize = true;
+            this.lblId.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblId.Location = new System.Drawing.Point(25, 278);
+            this.lblId.Name = "lblId";
+            this.lblId.Size = new System.Drawing.Size(19, 15);
+            this.lblId.TabIndex = 1;
+            this.lblId.Text = "ID";
+            // 
+            // txtId
+            // 
+            this.txtId.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtId.Location = new System.Drawing.Point(96, 275);
+            this.txtId.Name = "txtId";
+            this.txtId.Size = new System.Drawing.Size(100, 23);
+            this.txtId.TabIndex = 2;
+            // 
+            // txtSId
+            // 
+            this.txtSId.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtSId.Location = new System.Drawing.Point(96, 319);
+            this.txtSId.Name = "txtSId";
+            this.txtSId.Size = new System.Drawing.Size(100, 23);
+            this.txtSId.TabIndex = 4;
+            // 
+            // lblSId
+            // 
+            this.lblSId.AutoSize = true;
+            this.lblSId.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblSId.Location = new System.Drawing.Point(25, 322);
+            this.lblSId.Name = "lblSId";
+            this.lblSId.Size = new System.Drawing.Size(31, 15);
+            this.lblSId.TabIndex = 3;
+            this.lblSId.Text = "학번";
+            // 
+            // txtName
+            // 
+            this.txtName.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtName.Location = new System.Drawing.Point(96, 359);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 23);
+            this.txtName.TabIndex = 6;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblName.Location = new System.Drawing.Point(25, 362);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(31, 15);
+            this.lblName.TabIndex = 5;
+            this.lblName.Text = "이름";
+            // 
+            // txtPhone
+            // 
+            this.txtPhone.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtPhone.Location = new System.Drawing.Point(96, 402);
+            this.txtPhone.Name = "txtPhone";
+            this.txtPhone.Size = new System.Drawing.Size(100, 23);
+            this.txtPhone.TabIndex = 8;
+            // 
+            // lblPhone
+            // 
+            this.lblPhone.AutoSize = true;
+            this.lblPhone.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblPhone.Location = new System.Drawing.Point(25, 405);
+            this.lblPhone.Name = "lblPhone";
+            this.lblPhone.Size = new System.Drawing.Size(55, 15);
+            this.lblPhone.TabIndex = 7;
+            this.lblPhone.Text = "전화번호";
+            // 
+            // btnViewAll
+            // 
+            this.btnViewAll.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnViewAll.Location = new System.Drawing.Point(304, 275);
+            this.btnViewAll.Name = "btnViewAll";
+            this.btnViewAll.Size = new System.Drawing.Size(75, 23);
+            this.btnViewAll.TabIndex = 9;
+            this.btnViewAll.Text = "View All";
+            this.btnViewAll.UseVisualStyleBackColor = true;
+            // 
+            // btnDeleteAll
+            // 
+            this.btnDeleteAll.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnDeleteAll.Location = new System.Drawing.Point(430, 273);
+            this.btnDeleteAll.Name = "btnDeleteAll";
+            this.btnDeleteAll.Size = new System.Drawing.Size(75, 23);
+            this.btnDeleteAll.TabIndex = 10;
+            this.btnDeleteAll.Text = "모두삭제";
+            this.btnDeleteAll.UseVisualStyleBackColor = true;
+            this.btnDeleteAll.Click += new System.EventHandler(this.btnDeleteAll_Click);
+            // 
+            // btnInsert
+            // 
+            this.btnInsert.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnInsert.Location = new System.Drawing.Point(304, 322);
+            this.btnInsert.Name = "btnInsert";
+            this.btnInsert.Size = new System.Drawing.Size(75, 23);
+            this.btnInsert.TabIndex = 11;
+            this.btnInsert.Text = "추가";
+            this.btnInsert.UseVisualStyleBackColor = true;
+            this.btnInsert.Click += new System.EventHandler(this.btnInsert_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnDelete.Location = new System.Drawing.Point(430, 322);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(75, 23);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.Text = "삭제";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnSearch.Location = new System.Drawing.Point(304, 362);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 13;
+            this.btnSearch.Text = "검색";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnUpdate.Location = new System.Drawing.Point(430, 362);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(75, 23);
+            this.btnUpdate.TabIndex = 14;
+            this.btnUpdate.Text = "수정";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnClear.Location = new System.Drawing.Point(304, 405);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 15;
+            this.btnClear.Text = "clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("맑은 고딕", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnClose.Location = new System.Drawing.Point(430, 405);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 16;
+            this.btnClose.Text = "종료";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(583, 450);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnInsert);
+            this.Controls.Add(this.btnDeleteAll);
+            this.Controls.Add(this.btnViewAll);
+            this.Controls.Add(this.txtPhone);
+            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtSId);
+            this.Controls.Add(this.lblSId);
+            this.Controls.Add(this.txtId);
+            this.Controls.Add(this.lblId);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblId;
+        private System.Windows.Forms.TextBox txtId;
+        private System.Windows.Forms.TextBox txtSId;
+        private System.Windows.Forms.Label lblSId;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox txtPhone;
+        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Button btnViewAll;
+        private System.Windows.Forms.Button btnDeleteAll;
+        private System.Windows.Forms.Button btnInsert;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
     }
 }
 
--- 021. Firebase/Form1.cs
+++ 021. Firebase/Form1.cs
@@ -20,7 +20,8 @@ namespace _021.Firebase
             AuthSecret = "U1XJnaJ4HGIsXcactHPmOfD3PHNtqXDlN76F42JO",
             BasePath = "https://fir-hyejin-default-rtdb.firebaseio.com/"
         };
-        IFirebaseClient client;
+
+        IFirebaseClient client;    //database와 c#을 이어주는 매개체
         public Form1()
         {
             InitializeComponent();
@@ -33,6 +34,86 @@ namespace _021.Firebase
             {
                 MessageBox.Show("Connection 성공!");
             }
+        }
+
+        private async void btnInsert_Click(object sender, EventArgs e)
+        {
+            //Data 클래스를 만들것
+            var data = new Data
+            {
+                Id= txtId.Text,
+                SId= txtSId.Text,
+                Name = txtName.Text,
+                Phone = txtPhone.Text
+            };
+
+            SetResponse response = await client.SetAsync("VP02_Phonebook/" + txtId.Text, data);
+            Data result = response.ResultAs<Data>();
+            MessageBox.Show("Data Inserted : Id = " + result.Id);
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtId.Text = "";
+            txtSId.Text = "";
+            txtName.Text = "";
+            txtPhone.Text = "";
+
+        }
+
+        private async void btnSearch_Click(object sender, EventArgs e)
+        {
+            FirebaseResponse response = await client.GetAsync("VP02_Phonebook/" + txtId.Text);
+            Data obj = response.ResultAs<Data>();
+
+            txtId.Text = obj.Id;
+            txtSId.Text = obj.SId;
+            txtName.Text = obj.Name;
+            txtPhone.Text = obj.Phone;
+
+            MessageBox.Show("Data retrieved successfully!");
+        }
+
+        private async void btnUpdate_Click(object sender, EventArgs e)
+        {
+            var data = new Data
+            {
+                Id = txtId.Text,
+                SId = txtSId.Text,
+                Name = txtName.Text,
+                Phone = txtPhone.Text
+            };
+
+            FirebaseResponse response = await client.UpdateAsync("VP02_Phonebook/" + txtId.Text,data);
+            Data result = response.ResultAs<Data>();
+            MessageBox.Show("Data Updated at " + result.Id);
+        }
+
+        private async void btnDelete_Click(object sender, EventArgs e)
+        {
+            FirebaseResponse response = await client.DeleteAsync("VP02_Phonebook/" + txtId.Text);
+            MessageBox.Show("Deleted!");
+            txtId.Text = "";
+            txtSId.Text = "";
+            txtName.Text = "";
+            txtPhone.Text = "";
+        }
+
+        //모두 삭제: 메시지박스 확인 필요
+        private async void btnDeleteAll_Click(object sender, EventArgs e)
+        {
+            DialogResult result=MessageBox.Show("저장된 데이터가 모두 삭제됩니다. 계속할까요?", "Warning!", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+
+            if (result == DialogResult.No)
+                return;
+
+            FirebaseResponse response = await client.DeleteAsync("VP02_Phonebook");
+            MessageBox.Show("All Datd Deleted /VP02_Phonebook");
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }
 }
